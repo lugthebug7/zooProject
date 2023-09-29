074eb3e81ab15c9f0d162ae6c6152b233d26969c
lugthebug7 <lpotter1@ithaca.edu>
2023-09-28 20:05:13 -0400

    WE GOTTA WORKING TIMER

--- zoo_website/target/classes/zoo/website/script-tester.js
+++ zoo_website/target/classes/zoo/website/script-tester.js
@@ -1,19 +1,49 @@
-// script.js
-const circle = document.querySelector('.circle');
+// Get all the gate buttons
+const gateButtons = document.querySelectorAll(".gate-button");
 
-function depletePerimeter() {
-    const initialDashArray = 600; // Set to the circumference of the circle (2 * π * r)
-    let dashOffset = initialDashArray;
+// Add a click event listener to each gate button
+gateButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+        button.classList.toggle("flash");
+    });
+});
 
-    const timer = setInterval(() => {
-        if (dashOffset > 0) {
-            dashOffset -= 1; // Decrease dash offset to reveal the perimeter
-            circle.style.strokeDashoffset = dashOffset;
-        } else {
-            clearInterval(timer);
+
+var intervalId;
+
+function startTimer(duration) {
+    var time = duration;
+    var i = 1;
+    var k = 100 - ((i / duration) * 100); // Adjust for the initial stroke-dashoffset
+    var l = 100 - k;
+    i++;
+
+    document.getElementById("c2").style.strokeDasharray = [k, l];
+
+    document.getElementById("c2").style.strokeDashoffset = 25; // Set the initial stroke-dashoffset
+
+    intervalId = setInterval(function () {
+        if (i > time) {
+            clearInterval(intervalId);
+            return;
         }
-    }, 1000); // Adjust the interval for smoother animation
+        k = 100 - ((i / duration) * 100);
+        l = 100 - k;
+
+        document.getElementById("c2").style.strokeDasharray = [k, l];
+
+        document.getElementById("c2").style.strokeDashoffset = 25; // Maintain the same initial offset
+        console.log(k, l);
+        i++;
+    }, 1000);
 }
 
-// Start the depletion on page load or as needed
-window.addEventListener('load', depletePerimeter);
+startTimer(300);
+
+// Add an event listener to the button element
+document.querySelector(".food-button").addEventListener("click", function () {
+    clearInterval(intervalId); // Clear the interval to stop the timer
+    // Reset the timer by starting it again
+    startTimer(300);
+});
+
